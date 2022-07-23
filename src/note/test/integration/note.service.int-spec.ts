beccf3e648a4ef27c8861ecb63017ad21e0dfc95
Dmitry Trokhlib <fgh98zip@gmail.com>
2022-07-23 09:16:57 +0300

    refactor integration tests for Notes service

--- src/note/test/integration/note.service.int-spec.ts
+++ src/note/test/integration/note.service.int-spec.ts
@@ -1,13 +1,17 @@
 import { Test } from '@nestjs/testing';
-import { use } from 'passport';
 import { AppModule } from 'src/app.module';
 import { CreateNoteDto, UpdateNoteDto } from 'src/note/dto';
 import { NoteService } from 'src/note/note.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { createNoteDto, updateNoteDto, userDto } from './note.test.data';
 
 describe('NoteService Int', () => {
   let prismaService: PrismaService;
   let noteService: NoteService;
+
+  let userId: number;
+  let noteId: number;
+  const INVALID_ID: number = -1;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -25,163 +29,86 @@ describe('NoteService Int', () => {
   });
 
   describe('create Note', () => {
-    let userId, noteId;
     it('should create user', async () => {
       const user = await prismaService.user.create({
-        data: { email: 'test-int@test.com', hash: '123321' },
+        data: userDto,
       });
       userId = user.id;
     });
     it('should create NOTE', async () => {
-      const dto: CreateNoteDto = {
-        title: 'My test note',
-        description: 'My test note description',
-        relatedDate: '2022-07-20T19:28:44+00:00',
-      };
-      const createdNote = await noteService.createNote(userId, dto);
-      expect(createdNote.title).toBe(dto.title);
-      expect(createdNote.description).toBe(dto.description);
-      expect(createdNote.relatedDate).toBe(dto.relatedDate);
-
+      const createdNote = await noteService.createNote(userId, createNoteDto);
+      expect(createdNote.title).toBe(createNoteDto.title);
+      expect(createdNote.description).toBe(createNoteDto.description);
+      expect(createdNote.relatedDate).toBe(createNoteDto.relatedDate);
       noteId = createdNote.id;
     });
   });
 
   describe('should update NOTE', () => {
-    let userId, noteId;
-    it('should retrieve user', async () => {
-      const user = await prismaService.user.findUnique({
-        where: {
-          email: 'test-int@test.com',
-        },
-      });
-      userId = user.id;
-    });
-    it('should create NOTE', async () => {
-      const dto: CreateNoteDto = {
-        title: 'My test note',
-        description: 'My test note description',
-        relatedDate: '2022-07-20T19:28:44+00:00',
-      };
-      const createdNote = await noteService.createNote(userId, dto);
-      noteId = createdNote.id;
-    });
     it('should update NOTE', async () => {
-      const dto: UpdateNoteDto = {
-        title: 'My test note 2',
-        description: 'My test note description 2',
-        relatedDate: '2022-08-20T19:28:44+00:00',
-      };
-      const updatedTodo = await noteService.updateNoteById(userId, noteId, dto);
-      expect(updatedTodo.title).toBe(dto.title);
-      expect(updatedTodo.description).toBe(dto.description);
-      expect(updatedTodo.relatedDate).toBe(dto.relatedDate);
+      const updatedTodo = await noteService.updateNoteById(
+        userId,
+        noteId,
+        updateNoteDto,
+      );
+      expect(updatedTodo.title).toBe(updateNoteDto.title);
+      expect(updatedTodo.description).toBe(updateNoteDto.description);
+      expect(updatedTodo.relatedDate).toBe(updateNoteDto.relatedDate);
     });
-    it('should fail to update NOTE (Invalid user ID)', async () => {
-      const dto: UpdateNoteDto = {
-        title: 'My test note 2',
-        description: 'My test note description 2',
-        relatedDate: '2022-08-20T19:28:44+00:00',
-      };
-      const INVALID_ID = -1;
 
+    it('should fail to update NOTE (Invalid user ID)', async () => {
       await noteService
-        .updateNoteById(INVALID_ID, noteId, dto)
+        .updateNoteById(INVALID_ID, noteId, updateNoteDto)
         .then((note) => expect(note).toBeUndefined)
         .catch((error) => expect(error.status).toBe(403));
     });
     it('should fail to update NOTE (Invalid NOTE id)', async () => {
-      const dto: UpdateNoteDto = {
-        title: 'My test note 2',
-        description: 'My test note description 2',
-        relatedDate: '2022-08-20T19:28:44+00:00',
-      };
-      const INVALID_ID = -1;
-
       await noteService
-        .updateNoteById(userId, INVALID_ID, dto)
+        .updateNoteById(userId, INVALID_ID, updateNoteDto)
         .then((note) => expect(note).toBeUndefined)
         .catch((error) => expect(error.status).toBe(403));
     });
   });
 
   describe('should delete NOTE', () => {
-    let userId, noteId;
-    it('should retrieve user', async () => {
-      const user = await prismaService.user.findUnique({
-        where: {
-          email: 'test-int@test.com',
-        },
-      });
-      userId = user.id;
-    });
-    it('should create NOTE', async () => {
-      const dto: CreateNoteDto = {
-        title: 'My test note',
-        description: 'My test note description',
-        relatedDate: '2022-07-20T19:28:44+00:00',
-      };
-      const createdNote = await noteService.createNote(userId, dto);
-      noteId = createdNote.id;
-    });
-
     it('should fail to delete NOTE (Invalid User Id)', async () => {
-      const INVALID_ID = -1;
-
       await noteService
         .deleteNoteById(INVALID_ID, noteId)
         .then((note) => expect(note).toBeUndefined)
         .catch((error) => expect(error.status).toBe(403));
     });
     it('should fail to delete NOTE (Invalid NOTE Id)', async () => {
-      const INVALID_ID = -1;
-
       await noteService
         .deleteNoteById(userId, INVALID_ID)
         .then((note) => expect(note).toBeUndefined)
         .catch((error) => expect(error.status).toBe(403));
     });
-    it('should not return deleted note', async () => {
+    it('should return NOTE', async () => {
       await noteService.getNotesById(userId, noteId).then((note) => {
+        expect(note).toBeDefined();
         expect(note.id).toBe(noteId);
         expect(note.userId).toBe(userId);
       });
     });
-
-    it('should  delete NOTE', async () => {
+    it('should delete NOTE', async () => {
       const note = await noteService.deleteNoteById(userId, noteId);
       expect(note).toBeUndefined();
     });
-    it('should not return deleted note', async () => {
+    it('should not return deleted NOTE', async () => {
       await noteService
         .getNotesById(userId, noteId)
-        .then((note) => expect(note).toBeUndefined);
+        .then((note) => expect(note).toBeNull());
     });
   });
 
   describe('should retrieve NOTES', () => {
-    let userId,
-      INVALID_ID = -1;
-    it('should retrieve user', async () => {
-      const user = await prismaService.user.findUnique({
-        where: {
-          email: 'test-int@test.com',
-        },
-      });
-      userId = user.id;
-    });
-    it('should create NOTE', async () => {
-      const dto: CreateNoteDto = {
-        title: 'My test note',
-        description: 'My test note description',
-        relatedDate: '2022-07-20T19:28:44+00:00',
-      };
-      await noteService.createNote(userId, dto);
-      await noteService.createNote(userId, dto);
+    it('should create 2 NOTEs', async () => {
+      await noteService.createNote(userId, createNoteDto);
+      await noteService.createNote(userId, createNoteDto);
     });
     it('should retrieve ALL notes', async () => {
       const notes = await noteService.getNotes(userId);
-      expect(notes.length).toBeGreaterThan(1);
+      expect(notes.length).toBe(2);
     });
     it('should retrieve 0 notes', async () => {
       const notes = await noteService.getNotes(INVALID_ID);
@@ -190,24 +117,8 @@ describe('NoteService Int', () => {
   });
 
   describe('should retrieve NOTES by ID', () => {
-    let userId,
-      noteId,
-      INVALID_ID = -1;
-    it('should retrieve user', async () => {
-      const user = await prismaService.user.findUnique({
-        where: {
-          email: 'test-int@test.com',
-        },
-      });
-      userId = user.id;
-    });
     it('should create NOTE', async () => {
-      const dto: CreateNoteDto = {
-        title: 'My test note',
-        description: 'My test note description',
-        relatedDate: '2022-07-20T19:28:44+00:00',
-      };
-      const note = await noteService.createNote(userId, dto);
+      const note = await noteService.createNote(userId, createNoteDto);
       noteId = note.id;
     });
     it('should retrieve NOTE by Id', async () => {
---
+++ src/note/test/integration/note.test.data.ts
@@ -0,0 +1,13 @@
+import { CreateNoteDto, UpdateNoteDto } from 'src/note/dto';
+
+export const userDto = { email: 'test-int@test.com', hash: '123321' };
+export const createNoteDto: CreateNoteDto = {
+  title: 'My test note',
+  description: 'My test note description',
+  relatedDate: '2022-07-20T19:28:44+00:00',
+};
+export const updateNoteDto: UpdateNoteDto = {
+  title: 'My test note 2',
+  description: 'My test note description 2',
+  relatedDate: '2022-08-20T19:28:44+00:00',
+};
